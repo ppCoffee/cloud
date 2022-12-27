24007f5543d4247595e1f225a6f5f7a8affc6bdd
ppCoffee <646439950@qq.com>
2022-12-27 11:50:09 +0800

    1

--- spring_cloud/cloud-config-client-3366/src/main/java/com/evmtv/configCenter/ConfigClient3366Application.java
+++ spring_cloud/cloud-config-client-3366/src/main/java/com/evmtv/configCenter/ConfigClient3366Application.java
@@ -10,11 +10,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class ConfigClient3355Application {
+public class ConfigClient3366Application {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ConfigClient3355Application.class, args);
+		SpringApplication.run(ConfigClient3366Application.class, args);
 	}
 	
 }
