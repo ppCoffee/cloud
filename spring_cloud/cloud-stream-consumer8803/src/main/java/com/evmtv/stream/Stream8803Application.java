7f9475902556f33666eac87ac0b632a23472f735
ppCoffee <646439950@qq.com>
2022-12-27 21:33:42 +0800

    1

---
+++ spring_cloud/cloud-stream-consumer8803/src/main/java/com/evmtv/stream/Stream8803Application.java
@@ -0,0 +1,19 @@
+package com.evmtv.stream;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Stream8803Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Stream8803Application.class, args);
+	}
+	
+}
