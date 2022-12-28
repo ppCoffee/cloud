0098d4e00f3b4896e373eaf9b4842fed1bd25bf4
ppCoffee <646439950@qq.com>
2022-12-28 12:47:48 +0800

    1

---
+++ spring_cloud/cloudalibaba-config-nacos-client3377/src/main/java/com/evmtv/nacos/NacosConfig3377Application.java
@@ -0,0 +1,20 @@
+package com.evmtv.nacos;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfig3377Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NacosConfig3377Application.class, args);
+	}
+	
+}
---
+++ spring_cloud/cloudalibaba-config-nacos-client3377/src/main/java/com/evmtv/nacos/controller/ConfigClientContoller.java
@@ -0,0 +1,35 @@
+/**
+ * ConfigClientContoller.java
+ * Copyright(JAVA) EnRich DTV Group co.,Ltd
+ * 功能描述：
+ *   
+ * 创建者：zhangzy@evmtv.com 
+ * 编辑者: zhangzy@evmtv.com
+ * 2022年12月27日
+ */
+
+package com.evmtv.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigClientContoller {
+
+	@Value("${server.port}")
+	private String serverPort;
+	
+	@Value("${config}")
+	private String serverConfig;
+	
+	@GetMapping("/configInfo")
+	public String getConfigInfo() {
+		
+		return serverConfig + "\t\n\n" + " serverPort:" + serverPort;
+	}
+	
+	
+}
