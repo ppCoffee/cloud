23226aa8a4a8e2c0f0dfe2f69af432031a39830f
ppCoffee <646439950@qq.com>
2022-12-29 09:37:02 +0800

    1

---
+++ spring_cloud/cloudalibaba-sentinel-service8401/src/main/java/com/evmtv/sentinel/Sentinel8401Application.java
@@ -0,0 +1,20 @@
+package com.evmtv.sentinel;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sentinel8401Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Sentinel8401Application.class, args);
+	}
+	
+}
---
+++ spring_cloud/cloudalibaba-sentinel-service8401/src/main/java/com/evmtv/sentinel/controller/FlowLimitController.java
@@ -0,0 +1,35 @@
+/**
+ * FlowLimitController.java
+ * Copyright(JAVA) EnRich DTV Group co.,Ltd
+ * 功能描述：
+ *   
+ * 创建者：zhangzy@evmtv.com 
+ * 编辑者: zhangzy@evmtv.com
+ * 2022年12月29日
+ */
+
+package com.evmtv.sentinel.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FlowLimitController
+{
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+
+        return "------testB";
+    }
+
+
+
+}
+ 
